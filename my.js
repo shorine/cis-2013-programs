6d3054074fe5cd6a47b0178a2af29c72c64afbfe
shorine <54118290+shorine@users.noreply.github.com>
2021-08-30 13:29:52 -0500

    Add files via upload

--- my.js
+++ my.js
@@ -7,7 +7,7 @@ var mainProgram = function ()
 {
     var stringFirstName = prompt("What is your first name?");
     if (!isNaN(stringFirstName)) {
-        alert("You did not enter a valid first name, so I will call you Mickie Mouuse.  Hello Mickie!");
+        alert("You did not enter a valid first name, so I will call you Mickey.  Hello Mickey!");
     } else
     {
         alert("Hello "+ stringFirstName +"! It's great to meet you!");  
